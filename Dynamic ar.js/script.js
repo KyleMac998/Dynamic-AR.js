1e6aa4b5fe570277d1605ba22d711384018a8662
Kyle MacDonald <k.macdonald998@gmail.com>
2020-03-03 14:48:25 +0000

    Update script.js

--- Dynamic ar.js/script.js
+++ Dynamic ar.js/script.js
@@ -58,38 +58,36 @@ window.onload = () => {
                 });
             })
     },
-        (err) => console.error('Error in retrieving position', err),
+
+    // this click listener has to be added simply to a click event on an a-entity element
+const clickListener = function (ev) {
+    ev.stopPropagation();
+    ev.preventDefault();
+
+    const name = ev.target.getAttribute('name');
+    const el = ev.detail.intersection && ev.detail.intersection.object.el;
+
+    if (el && el === ev.target) {
+        // after click, we are adding a label with the name of the place
+        const label = document.createElement('span');
+        const container = document.createElement('div');
+        container.setAttribute('id', 'place-label');
+        label.innerText = name;
+        container.appendChild(label);
+        document.body.appendChild(container);
+
+        setTimeout(() => {
+            // that will disappear after less than 2 seconds
+            container.parentElement.removeChild(container);
+        }, 1500);
+     }
+ };
+
+    (err) => console.error('Error in retrieving position', err),
         {
             enableHighAccuracy: true,
             maximumAge: 0,
             timeout: 27000,
-
-            icon.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
-
-       const clickListener = function (ev) {
-           ev.stopPropagation();
-           ev.preventDefault();
-
-           const name = ev.target.getAttribute('name');
-
-           const el = ev.detail.intersection && ev.detail.intersection.object.el;
-
-           if (el && el === ev.target) {
-               const label = document.createElement('span');
-               const container = document.createElement('div');
-               container.setAttribute('id', 'place-label');
-               label.innerText = name;
-               container.appendChild(label);
-               document.body.appendChild(container);
-
-               setTimeout(() => {
-                   container.parentElement.removeChild(container);
-               }, 1500);
-           }
-       };
-
-       icon.addEventListener('click', clickListener);
-
-       scene.appendChild(icon);
-   });
-}
+        }
+    );
+};
